3410e28b2deb2c1323f0058273e8da4b168017a6
Hamshini Kannan <hamshini.kannan@aspiresys.com>
2021-07-08 21:10:19 +0530

    Validation

--- validation.js
+++ validation.js
@@ -1,43 +1,39 @@
-
-    function validate(){
-  var name = document.getElementById("name").value;
-  var amount = document.getElementById("amount").value;
-  var date = document.getElementById("date").value;
-  var email = document.getElementById("email").value;
-  atpos = email.indexOf("@");
-  dotpos = email.lastIndexOf(".");
-  var message = document.getElementById("message").value;
-  var error_message = document.getElementById("error_message");
+function validate(){
+    var name = document.getElementById("name").value;
+    var amount = document.getElementById("amount").value;
+    var date = document.getElementById("date").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
   
-  error_message.style.padding = "10px";
+    error_message.style.padding = "10px";
   
-  var text;
-  if(name.length < 3){
-    text = "Please Enter valid Name";
-    error_message.innerHTML = text;
-    return false;
-  }
-  if(amount.length<4){
-      text = "Please Enter valid Amount";
-      error_message.innerHTML = text;
-      return false;
-  }
-  if (date == null || date == "" ) {
-        text = "Please Enter Date of Birth  as DD-MM-YYYY";
+    var text;
+    if(name.length < 3 || !isNaN(name)){
+        text = "Please Enter valid Name";
         error_message.innerHTML = text;
         return false;
     }
-   
-  if(email != /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/){
-    text = "Please Enter valid Email";
-    error_message.innerHTML = text;
-    return false;
-  }
-  if(message.length !=0){
-    text = "Please share your thought";
-    error_message.innerHTML = text;
-    return false;
-  }
-  alert("Form Submitted Successfully!");
-  return true;
+    if(amount.length<4){
+        text = "Please Enter valid Amount";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (date == null || date == "" ) {
+        text = "Please Enter Date of Birth ";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6 || !(/\w+\d*@\w+.\w+/.test(email)) ){
+        text = "Please Enter valid Email";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(message.length <= 10 || message.length ==0){
+        text = "Please Enter More Than 10 Characters";
+        error_message.innerHTML = text;
+        return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
 }
