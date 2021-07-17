f1cbd170e1c57fd80aa71f5b57423abff03f0c0a
hamshinikannan <86414200+hamshinikannan@users.noreply.github.com>
2021-07-17 11:59:25 +0530

    php files

--- Array.php
+++ Array.php
@@ -1,21 +1,21 @@
 <?php
-$array = array(
+$array = array[
     1    => "abc",
     "1"  => "be",
     1.9  => "c",
     true => "dedhee",
     
-);
+];
 var_dump($array);
 ?><br>
 <?php
-$array = array("foo", "bar", "hello", "world");
+$array = array["foo", "bar", "hello", "world"];
 var_dump($array);
 ?>
 
 <?php
 //use of unset function
-$arr = array(5 => 1, 12 => 2);
+$arr = array[5 => 1, 12 => 2];
 $arr[] = 56;    // This is the same as $arr[13] = 56;
 $arr["x"] = 42; // This adds a new element to the array with key "x"
 unset($arr[5]); // This removes the element from the array
@@ -23,7 +23,7 @@ unset($arr);    // This deletes the whole array
 ?>
 
 <?php
-$a = array(1 => 'one', 2 => 'two', 3 => 'three');
+$a = array[1 => 'one', 2 => 'two', 3 => 'three'];
 unset($a[2]);
 //array_values- reindex the array
 $b = array_values($a); // Now $b is array(0 => 'one', 1 =>'three')
@@ -31,7 +31,7 @@ $b = array_values($a); // Now $b is array(0 => 'one', 1 =>'three')
 
 <?php
 //key value of each array element
-$switching = array( 
+$switching = array[ 
                             10, // key = 0
                     5    =>  6,
                     3    =>  7, 
@@ -40,5 +40,5 @@ $switching = array(
                     '8'  =>  2, // key = 8 (integer!)
                     '02' => 77, // key = '02'
                     0    => 12  // the value 10 will be overwritten by 12
-                  );
+                  ];
 ?>
