dbb3bcb3fd20f0c5a2e65cd97b7353d802421698
hamshinikannan <86414200+hamshinikannan@users.noreply.github.com>
2021-07-17 12:02:39 +0530

    Update control_structure.php

--- control_structure.php
+++ control_structure.php
@@ -76,10 +76,15 @@ for ($i = 0; $i < 5; ++$i) {
 switch ($i) {
     case 0:
         echo "i equals 0";
+        break;
     case 1:
         echo "i equals 1";
+        break;
     case 2:
         echo "i equals 2";
+        break;
+  default:
+        echo "i is not equal to 0,1 and 2"; 
 }
 
 //match
